6ea8aaad9713c6ef64151fad2e24553906c9b227
Grigory Petrenko <grepetro@gmail.com>
2023-09-14 09:43:00 -0300

    v7.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,10 +1,15 @@
 import os
 from pathlib import Path
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
